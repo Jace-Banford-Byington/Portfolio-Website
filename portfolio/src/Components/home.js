ee039c24dd06b93ceae3d09523982b7ec85a3ff0
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-05-14 11:49:57 -0600

    Sass

---
+++ portfolio/src/Components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './home.scss';
+
+const Home = () => {
+
+    return (
+        <div className="">
+            <p>Hello there, I am Jace</p>
+        </div>
+    )
+}
+
+
+
+
+export default Home;
